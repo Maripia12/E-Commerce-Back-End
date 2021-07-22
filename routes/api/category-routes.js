0ec71afe568a7842bb8a736de800204f42473463
Pia Paulino <maripia1220@gmail.com>
2021-07-22 16:05:44 -0400

    Updated category router GET

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,17 +1,42 @@
 const router = require('express').Router();
+const { json } = require('sequelize/types');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try{
+    const categoryData = await Category.findAll({
+      include:[{model:Product}],
+    });
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
   // find all categories
   // be sure to include its associated Products
 });
+  
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
+  try{
+    const categoryData = await categoryData.findOne(
+      req.params.id, {include: [{model:Product}]
+    }),
+    if(!categoryData){
+      res.status(404).json({message:'No category found with this id!'});
+      return;
+    }
+     res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+    // find one category by its `id` value
+    // be sure to include its associated Products
+  });
+
+
+
 
 router.post('/', (req, res) => {
   // create a new category
